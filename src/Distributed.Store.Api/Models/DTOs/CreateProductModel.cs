31575abf5ae182ea108d864a84ad4031835cd947
Alexei <alexei.quiroga@wallit.eu>
2023-06-06 18:54:56 -0400

    Initial product controller

---
+++ src/Distributed.Store.Api/Models/DTOs/CreateProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Distributed.Store.Api.Models.DTOs
+{
+    public class CreateProductModel
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
