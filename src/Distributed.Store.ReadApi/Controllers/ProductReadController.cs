8977a9ce23a3e0efe67f2fab76d48fbdfe4b654c
Alexei <alexei.quiroga@wallit.eu>
2023-06-27 15:35:34 -0400

    separate api in read and write api to apply cqrs

---
+++ src/Distributed.Store.ReadApi/Controllers/ProductReadController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Distributed.Store.ReadApi.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductReadController : ControllerBase
+    {
+        public ProductReadController()
+        {
+
+        }
+        [Route("getProduct")]
+        [HttpGet]
+        public IActionResult GetProduct(int id)
+        {
+            return Ok();
+        }
+    }
+}
